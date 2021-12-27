727d1578d0a8c06e2636e5645dea7c59970c8930
Jeongwon Kim <55073876+gardenk11181@users.noreply.github.com>
2021-12-27 16:43:50 +0900

    dataset class for adult, german added

---
+++ dataset.py
@@ -0,0 +1,49 @@
+import torch
+import os
+import pickle
+
+from torch.utils.data import Dataset
+
+
+class Adult(Dataset):
+    def __init__(self, dir_path, train=True):
+        if train:
+            self.path = os.path.join(dir_path, 'adult_train.pkl')
+        else:
+            self.path = os.path.join(dir_path, 'adult_test.pkl')
+        with open(self.path, 'rb') as f:
+            self.data = pickle.load(f)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        data = self.data.iloc[index]
+        y = torch.as_tensor(data.pop('label'), dtype=torch.float32).reshape(-1)
+        s = torch.as_tensor(data.pop('age'), dtype=torch.float32).reshape(-1)
+        x = torch.as_tensor(data, dtype=torch.float32)
+
+        # x: [batch_size, 102], s&y: [batch_size, 1]
+        return [x, s, y]
+
+
+class German(Dataset):
+    def __init__(self, dir_path, train=True):
+        if train:
+            self.path = os.path.join(dir_path, 'german_train.pkl')
+        else:
+            self.path = os.path.join(dir_path, 'german_test.pkl')
+        with open(self.path, 'rb') as f:
+            self.data = pickle.load(f)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        data = self.data.iloc[index]
+        y = torch.as_tensor(data.pop('label'), dtype=torch.float32).reshape(-1)
+        s = torch.as_tensor(data.pop('sex'), dtype=torch.float32).reshape(-1)
+        x = torch.as_tensor(data, dtype=torch.float32)
+
+        # x: [batch_size, 56], s&y: [batch_size, 1]
+        return [x, s, y]
