5452bfaf9fa42af7c1532f8d3861a13b1b252b9a
Jeongwon Kim <55073876+gardenk11181@users.noreply.github.com>
2022-01-09 14:40:26 +0900

    amazon dataset preprocessing & loader added

--- dataset.py
+++ dataset.py
@@ -20,7 +20,7 @@ class Adult(Dataset):
     def __getitem__(self, index):
         data = self.data.iloc[index]
         y = torch.as_tensor(data.pop('label'), dtype=torch.float32).reshape(-1)
-        s = torch.as_tensor(data.pop('age'), dtype=torch.float32).reshape(-1)
+        s = torch.as_tensor(data.pop('sex'), dtype=torch.float32).reshape(-1)
         x = torch.as_tensor(data, dtype=torch.float32)
 
         # x: [batch_size, 121], s&y: [batch_size, 1]
@@ -69,3 +69,33 @@ class Health(Dataset):
 
         # x: [batch_size, 621], s&y: [batch_size, 1]
         return [x, s, y]
+
+
+class Amazon(Dataset):
+    def __init__(self, dir_path, source, target, tp=0):
+        """
+        :param dir_path: ~/amazon
+        :param source: source domain e.g. books, dvd
+        :param target: target domain e.g. electronics, kitchen
+        :param tp: 0 -> source train, 1 -> target train, 2-> target test
+        """
+        self.path = os.path.join(dir_path, '%s_to_%s.pkl' % (source, target))
+        self.tp = tp
+        with open(self.path, 'rb') as r:
+            self.data = pickle.load(r)[self.tp]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        data = self.data.iloc[index]
+        if self.tp == 1:
+            x = torch.as_tensor(data, dtype=torch.float32)
+            # x: [batch_size, 5000]
+            return [x]
+
+        else:
+            y = torch.as_tensor(data.pop('y'), dtype=torch.float32).reshape(-1)
+            x = torch.as_tensor(data, dtype=torch.float32)
+            # x: [batch_size, 5000], y: [batch_size, 1]
+            return [x, y]
