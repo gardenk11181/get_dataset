e18fe8dcc251ae9a21f25e333b5864ea5352f38c
Jeongwon Kim <gardenk11181@snu.ac.kr>
2022-01-10 14:39:31 +0900

    dir_path added

--- test.py
+++ test.py
@@ -11,18 +11,17 @@ import pickle
 if __name__ == '__main__':
     warnings.filterwarnings(action='ignore')
 
-    german = GermanPrepare('german', 'german_source')
-    german.prepare()
+    dir_path = '/Volumes/GoogleDrive/내 드라이브/VFAE'
 
     dict = {0: 'german', 1: 'adult', 2: 'health'}
-    dict_model = {0: 'x', 1: 'vae', 2: 'vfae'}
-    for i in [0]:
-        z, s, y = preprocess(i)
+    dict_model = {0: 'x', 1: 'vfae'}
+    for i in [0, 1, 2]:
+        z, s, y = preprocess(i, dir_path)
         [log_scores, rf_scores, log_y_scores, disc_scores, disc_prob_scores] = calculate(z, s, y)
         print('---------------------------------------------------------------------------')
         print(dict[i])
         print('---------------------------------------------------------------------------')
-        for j in range(1):
+        for j in range(2):
             print('     %s' % dict_model[j])
             print('     logistic: %f, random forest: %f' % (log_scores[j], rf_scores[j]))
             print('     discrimination on s: %f, discrimination prob on s: %f' % (disc_scores[j], disc_prob_scores[j]))
